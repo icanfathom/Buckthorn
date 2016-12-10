fd648b64a2041b4a284c5b86ebcb7370d823e68e
Unknown <joseph.merboth@gmail.com>
2016-12-09 22:24:35 -0600

    Begin creating observation add/edit page. Fix database connection in observations.php.

--- observations.php
+++ observations.php
@@ -1,24 +1,23 @@
 <?php
 	//header.php provides $con, a connection to the db
 include 'header.php';
+
+	$observations = run_sql_string("SELECT * FROM observation NATURAL JOIN team");
 ?>
 
-<h1>Observations</h1>
+</div> <!-- Close the container div that header.php starts so that this page can fill the whole screen (instead of being constrained to a smaller width). -->
 
-<?php
-	$obsQuery = "SELECT * FROM observation NATURAL JOIN team";
-	$obsResult = mysqli_query($con, $obsQuery);
-?>
+<h3>Observations</h3>
 
 <ul class="collapsible" data-collapsible="expandable">
-	<?php while ($obsRow = mysqli_fetch_array($obsResult)): ?>
+	<?php foreach ($observations as $obs): ?>
 		<li>
 			<div class="collapsible-header">
 				<ul>
-					<li class="date" ><?php echo $obsRow[date]; ?></li>
-					<li class="teamName" ><?php echo $obsRow[team_name]; ?></li>
-					<li class="numStems" ><?php echo "$obsRow[num_bt_stems] stems"; ?></li>
-					<li class="notes" ><?php echo $obsRow[description]; ?></li>
+					<li class="date" ><?php echo $obs['date']; ?></li>
+					<li class="teamName" ><?php echo $obs['team_name']; ?></li>
+					<li class="numStems" ><?php echo "{$obs['num_bt_stems']} stems"; ?></li>
+					<li class="notes" ><?php echo $obs['description']; ?></li>
 				</ul>
 			</div>
 			<div class="collapsible-body">
@@ -39,51 +38,50 @@ include 'header.php';
 					</tr>
 					<tr>
 						<td>
-							<?php echo "<a href=\"teams.php?team_id=$obsRow[team_id]\">$obsRow[team_name]</a>"; ?>
+							<?php echo "<a href=\"teams.php?team_id={$obs['team_id']}\">{$obs['team_name']}</a>"; ?>
 						</td>
 						<td>
-							<?php echo "$obsRow[coord_n] N, $obsRow[coord_w] W"; ?>
+							<?php echo "{$obs['coord_n']} N, {$obs['coord_w']} W"; ?>
 						</td>
 						<td>
-							<?php echo $obsRow[quadrat_size]; ?>
+							<?php echo $obs['quadrat_size']; ?>
 						</td>
 						<td>
-							<?php echo $obsRow[num_bt_stems]; ?>
+							<?php echo $obs['num_bt_stems']; ?>
 						</td>
 						<td>
-							<?php echo $obsRow[num_bt_stems] / $obsRow[quadrat_size]; ?>
+							<?php echo $obs['num_bt_stems'] / $obs['quadrat_size']; ?>
 						</td>
 						<td>
-							<?php echo $obsRow[follar_coverage]; ?>
+							<?php echo $obs['follar_coverage']; ?>
 						</td>
 						<td>
-							<?php echo $obsRow[median_bt_circumference]; ?>
+							<?php echo $obs['median_bt_circumference']; ?>
 						</td>
 						<td>
-							<?php echo $obsRow[description]; ?>
+							<?php echo $obs['description']; ?>
 						</td>
 						<td>
-							<a href="<?php echo $obsRow[photo_link]; ?>">Photos</a>
+							<a href="<?php echo $obs['photo_link']; ?>">Photos</a>
 						</td>
 						<td>
-							<?php echo $obsRow[obs_notes]; ?>
+							<?php echo $obs['obs_notes']; ?>
 						</td>
 					</tr>
 				</table>
 
 				<!----------Biodiversity---------->
 				<?php
-					$bioQuery = "SELECT * FROM biodiversity WHERE obs_id = $obsRow[obs_id]";
-					$bioResult = mysqli_query($con, $bioQuery);
-					$bioRow = mysqli_fetch_array($bioResult);
+					$bio = run_sql_string("SELECT * FROM biodiversity WHERE obs_id = {$obs['obs_id']}");
+					$bio = $bio[0];
 
-					$speciesQuery = "SELECT * FROM bio_species WHERE obs_id = $obsRow[obs_id] ORDER BY species";
-					$speciesResult = mysqli_query($con, $speciesQuery);
+					$speciesResults = run_sql_string("SELECT * FROM bio_species WHERE obs_id = {$obs['obs_id']} ORDER BY species");
 
 					//Throw together the string of surrounding species
-					$bio_string = "A ($bioRow[num_bt])";
-					while ($speciesRow = mysqli_fetch_array($speciesResult)) {
-						$bio_string .= ", $speciesRow[species] ($speciesRow[num])";
+					$bio_string = "A ({$bio['num_bt']})";
+					foreach ($speciesResults as $species)
+					{
+						$bio_string .= ", {$species['species']} ({$species['num']})";
 					}
 				?>
 				<div class="card">
@@ -92,39 +90,40 @@ include 'header.php';
 						<ul>
 							<li>Biodiversity: <?php echo $bio_string ?></li>
 							<li>Shannon-Weiner index: TODO</li>
-							<li>Notes: <?php echo $bioRow[bio_notes]; ?></li>
+							<li>Notes: <?php echo $bio['bio_notes']; ?></li>
 						</ul>
 					</div>
 				</div>
 
 				<!----------Competition---------->
 				<?php
-					$compQuery = "SELECT * FROM competition WHERE obs_id = $obsRow[obs_id]";
-					$compResult = mysqli_query($con, $compQuery);
+					$compResults = run_sql_string("SELECT * FROM competition WHERE obs_id = {$obs['obs_id']}");
 				?>
 				<div class="card">
 					<div class="card-content">
 						<span class="card-title">Competition</span>
 						<ul class="collection">
-							<?php while ($compRow =  mysqli_fetch_array($compResult)): ?>
+							<?php foreach ($compResults as $comp): ?>
 								<li class="collection-item">
 									<ul class="inline competition">
-										<li>BT <?php echo $compRow[bt_index]; ?></li>
-										<li>DBH: <?php echo $compRow[dbh]; ?></li>
-										<li>Dist to nearest BT: <?php echo $compRow[dist_to_nearest_bt]; ?></li>
-										<li>Nearest BT DBH: <?php echo $compRow[nearest_bt_dbh]; ?></li>
-										<li>Dist to nearest Non-BT: <?php echo $compRow[dist_to_nearest_non_bt]; ?></li>
-										<li>Nearest Non-BT DBH: <?php echo $compRow[nearest_non_bt_dbh]; ?></li>
-										<li>Notes: <?php echo $compRow[cmp_notes]; ?></li>
+										<li>BT <?php echo $comp['bt_index']; ?></li>
+										<li>DBH: <?php echo $comp['dbh']; ?></li>
+										<li>Dist to nearest BT: <?php echo $comp['dist_to_nearest_bt']; ?></li>
+										<li>Nearest BT DBH: <?php echo $comp['nearest_bt_dbh']; ?></li>
+										<li>Dist to nearest Non-BT: <?php echo $comp['dist_to_nearest_non_bt']; ?></li>
+										<li>Nearest Non-BT DBH: <?php echo $comp['nearest_non_bt_dbh']; ?></li>
+										<li>Notes: <?php echo $comp['cmp_notes']; ?></li>
 									</ul>
 								</li>
-							<?php endwhile; ?>
+							<?php endforeach; ?>
 						</ul>
 					</div>
 				</div>
 			</div>
 		</li>
-	<?php endwhile; ?>
+	<?php endforeach; ?>
 </ul>
+
+<div> <!-- Open an empty div because footer.php expects to close one. -->
 
 <?php include 'footer.php'; ?>
